64e3a19876d8b8c3ff1a3234ed134554b1b26ed2
Zecong Liang <liang312@purdue.edu>
2020-09-29 22:17:31 -0400

    Added temp sensor test code

---
+++ Firmware/Test code/Temperature_Test/Temperature_Test.ino
@@ -0,0 +1,22 @@
+int tempLogic = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  analogReference(EXTERNAL);
+}
+
+void loop()
+{
+ 
+  Serial.print(getTemp()); Serial.println(" degrees F");
+}
+
+float getTemp()
+{
+  int tempRead = analogRead(tempLogic);
+  float tempC = ((tempRead * 3.3 / 1024) - 0.5 ) * 100;
+  float tempF = tempC * 1.8 + 32;
+  
+  return tempF;
+}
